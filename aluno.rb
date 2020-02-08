1679f817c61ec29efbe4590d4adbc32939af83c0
Gabriel Sugai <gabrielsugai@gmail.com>
2020-02-08 16:43:55 -0500

    adicionando os primeiros atributos

--- aluno.rb
+++ aluno.rb
@@ -1,2 +1,7 @@
 class Aluno
+  def initialize(nome, telefone, matricula)
+    @nome = nome
+    @telefone = telefone
+    @matricula = matricula
+  end
 end
--- ex1_ruby_OO.rb
+++ ex1_ruby_OO.rb
@@ -1,3 +1,3 @@
 require_relative 'aluno'
-gabriel = Aluno.new
+gabriel = Aluno.new('Gabriel', '123456789', 22222)
 puts(gabriel)
