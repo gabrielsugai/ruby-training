26183e663e99ec324e682da9a901c5ad7f9b7601
Gabriel Sugai <gabrielsugai@gmail.com>
2020-02-08 19:22:15 -0500

    utilizando attr_accessor no lugar de metodos como readers e writtes

--- aluno.rb
+++ aluno.rb
@@ -1,18 +1,8 @@
 class Aluno
+  attr_accessor :nome, :telefone, :matricula
   def initialize(nome, telefone, matricula)
     @nome = nome
     @telefone = telefone
     @matricula = matricula
   end
-
-  # Get
-  def telefone
-    return @telefone
-  end
-
-  #Set
-  def telefone=(valor)
-    @telefone = valor
-  end
-
 end
