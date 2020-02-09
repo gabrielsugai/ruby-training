461d9251d9fdccbfce03ad0c8d47b6bb3ddc9224
Gabriel Sugai <gabrielsugai@gmail.com>
2020-02-08 21:55:34 -0500

    primeiras interações entre objetos

--- ex1_ruby_OO.rb
+++ ex1_ruby_OO.rb
@@ -1,14 +1,15 @@
 require_relative 'aluno'
 require_relative 'professor'
 require_relative 'turma'
-gabriel = Aluno.new('Gabriel', '123456789', 22222)
+gabriel = Aluno.new('Gabriel', '1234', 222)
 jose = Professor.new('Jose', 123, 'matematica')
-turmaA = Turma.new('Turma A', 10, 'Jose')
+turmaA = Turma.new('Turma A')
 
-puts("Aluno #{gabriel.nome} pertece a sala #{turmaA.nome} com o professor #{jose.nome}")
+joao = Aluno.new('Joao', '2315', 223)
+caio = Aluno.new('Caio', '4628', 224)
 
-jose.inicia_ferias()
-puts("Professor #{jose.nome} iniciou as ferias em #{jose.data_inicio_ferias}")
-puts('passando o tempo...')
-jose.encerra_ferias()
-puts("Professor #{jose.nome} encerrou as ferias em #{jose.data_fim_ferias}")
+turmaA.adiciona_aluno(gabriel)
+turmaA.adiciona_aluno(joao)
+turmaA.adiciona_aluno(caio)
+
+puts("A #{turmaA.nome} possui #{turmaA.total_alunos}")
--- turma.rb
+++ turma.rb
@@ -1,8 +1,13 @@
 class Turma
-  attr_accessor :nome, :sala, :professor
-  def initialize(nome, sala, professor)
+  attr_accessor :nome, :alunos
+  def initialize(nome)
     @nome = nome
-    @sala = sala
-    @professor = professor
+    @alunos=[]
+  end
+  def adiciona_aluno(aluno)
+    alunos << aluno
+  end
+  def total_alunos()
+    puts("Essa turma tem #{alunos.length} alunos")
   end
 end
