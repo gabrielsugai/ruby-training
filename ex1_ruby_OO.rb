9198b49077e418dd440efc385d4a27bcaefe7383
Gabriel Sugai <gabrielsugai@gmail.com>
2020-02-08 20:28:43 -0500

    primeiras interações com os objetos

--- ex1_ruby_OO.rb
+++ ex1_ruby_OO.rb
@@ -2,7 +2,11 @@ require_relative 'aluno'
 require_relative 'professor'
 require_relative 'turma'
 gabriel = Aluno.new('Gabriel', '123456789', 22222)
-jose = Professor.new('Jose', 123)
+jose = Professor.new('Jose', 123, 'matematica')
 turmaA = Turma.new('Turma A', 10, 'Jose')
 
 puts("Aluno #{gabriel.nome} pertece a sala #{turmaA.nome} com o professor #{jose.nome}")
+
+jose.inicia_ferias()
+
+puts(jose.inspect)
--- professor.rb
+++ professor.rb
@@ -1,7 +1,15 @@
 class Professor
+  attr_reader :ferias
   attr_accessor :nome, :codigo
-  def initialize(nome, codigo)
+  def initialize(nome, codigo, disciplina)
     @nome = nome
     @codigo = codigo
+    @disciplina = disciplina
+    @ferias = false
   end
+
+  def inicia_ferias()
+    @ferias = true
+  end
+
 end
